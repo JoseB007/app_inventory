f191a88d10faeb9f14e7cbb81aa08dc64ef15b8c
JoseB007 <JoseB007@users.noreply.github.com>
2025-03-03 17:07:23 -0500

    Arreglar funcion al guardar superuser

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -27,7 +27,7 @@ class Usuario(AbstractUser):
             if user.password != self.password:
                 self.set_password(self.password)
         
-        if not self.pk:
+        if not self.pk and not self.is_superuser:
             self.set_password(self.password)
 
         super().save(*args, **kwargs)
